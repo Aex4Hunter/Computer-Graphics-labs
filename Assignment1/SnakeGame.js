30871cda9f419569b2528477b191d2912cd72086
acn247 <Aleksandra.Akchurina@haw-hamburg.de>
2021-11-08 18:55:17 +0100

    created playground

--- Assignment1/SnakeGame.js
+++ Assignment1/SnakeGame.js
@@ -1,5 +1,10 @@
 /* global THREE */
 
+const planeSize = 10;
+const gridSize = 10;
+const gridDivisions = 10;
+const positionStep = 0.5;
+const snakeBuildBlock = 1;
 
 // * Initialize webGL
 const canvas = document.getElementById("myCanvas");
@@ -14,6 +19,88 @@ scene.add(camera);
 
 camera.lookAt(scene.position);
 scene.add(new THREE.AxesHelper(1.5));
+
+// Game content starts
+
+//helping functions
+const generateRandomLocation = () => Math.floor(Math.random() * (gridDivisions / 2 - -gridDivisions / 2 ) + -gridDivisions / 2);
+function snakeHandler(event) {
+ if(event.keyCode == 37 && (snakeParts[0].position.x > -gridDivisions/2+1)) {
+  snakeParts[0].position.x -=1;
+ } else if(event.keyCode == 38 && (snakeParts[0].position.y < gridDivisions/2 -1)) {
+  snakeParts[0].position.y +=1;
+ } else if(event.keyCode == 39 && (snakeParts[0].position.x < gridDivisions/2 -1)) {
+  snakeParts[0].position.x +=1;
+ } else if(event.keyCode == 40 && (snakeParts[0].position.y > -gridDivisions/2 +1)) {
+  snakeParts[0].position.y -=1;
+ }
+// console.log(event.keyCode);
+  
+}
+
+
+document.addEventListener('keydown', snakeHandler);
+
+//Plane & Grid
+const plane = new THREE.PlaneGeometry(planeSize, planeSize);
+const material = new THREE.MeshBasicMaterial( {color: '#118ab2', side: THREE.DoubleSide} );
+const gameField = new THREE.Mesh( plane, material );
+scene.add(gameField);
+
+const gridHelper = new THREE.GridHelper(gridSize, gridDivisions, '#06d6a0', '#06d6a0');
+console.log(gridHelper.position);
+gridHelper.rotation.x = Math.PI / 2;
+gridHelper.colorGrid = 'blue';
+scene.add(gridHelper);
+
+//Snake
+const snakeParts = [];
+
+function generateSnakePart() {
+  const geoCube = new THREE.BoxGeometry(snakeBuildBlock, snakeBuildBlock, snakeBuildBlock);
+  const snakeMaterial = new THREE.MeshBasicMaterial( {color: '#ef476f'} );
+  const snakePart = new THREE.Mesh(geoCube, snakeMaterial);
+
+  if(snakeParts.length === 0) {
+    const x = generateRandomLocation();
+    const y = generateRandomLocation();
+    
+    snakePart.position.set(x + positionStep,y + positionStep,positionStep);
+  }
+  
+  snakeParts.push(snakePart);
+  scene.add(snakePart);
+}
+
+function generateBall() {
+  const geoBall = new THREE.SphereGeometry(positionStep);
+  const ballMaterial = new THREE.MeshBasicMaterial( {color: '#ffd166'} );
+  const ballPart = new THREE.Mesh(geoBall, ballMaterial);
+
+  //create function GenerateRandomNumber
+  let x = generateRandomLocation();
+  let y = generateRandomLocation();
+  
+  for(const part of snakeParts) {
+    while(part.position.x == x && part.position.y == y) {
+      x = generateRandomLocation();
+      y = generateRandomLocation();
+    }
+  }
+  ballPart.position.set(x + positionStep,y + positionStep,positionStep);
+  scene.add(ballPart);
+}
+
+generateSnakePart();
+generateBall();
+
+
+// const geoCube = new THREE.BoxGeometry(1,1,1);
+// const snakeMaterial = new THREE.MeshBasicMaterial( {color: '#ef476f'} );
+// const snakePart = new THREE.Mesh(geoCube, snakeMaterial);
+// snakePart.position.set(positionStep,positionStep,positionStep);
+// scene.add(snakePart);
+
 
 // * Render loop
 const controls = new THREE.TrackballControls(camera, renderer.domElement);
